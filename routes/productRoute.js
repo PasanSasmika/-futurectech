ecba1be16b9b1e525f8961c61c14a701c85b2101
PasanSasmika <pasansasmika198@gmail.com>
2025-06-10 20:07:46 +0530

    Successfully implemented product creation function 👍

---
+++ routes/productRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { createProduct } from '../controllers/productController.js';
+
+
+
+const productRouter = express.Router();
+
+productRouter.post("/",createProduct)
+
+
+export default productRouter;
