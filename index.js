d047cdc55b037160e6d115e2de0e01939d0a9b0f
PasanSasmika <pasansasmika198@gmail.com>
2025-06-10 07:40:52 +0530

    use .env to configure database connection ≡ƒæì

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import "dotenv/config";
 
 
 const app = express();
+
+const mongoUrl = process.env.MONGODB_URI
+
+mongoose.connect(mongoUrl,{})
+
+const connection = mongoose.connection;
+
+connection.once("open",()=>{
+  console.log("Database connected");
+})
 
 app.listen(
   5000,
