cf51a11016499c193eee4cbfa1f8837262623a23
PasanSasmika <pasansasmika198@gmail.com>
2025-06-10 08:44:46 +0530

    Implement User Login 👤

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { userRegistration } from '../controllers/userController.js';
+import { loginUser, userRegistration } from '../controllers/userController.js';
 
 
 
 const userRouter = express.Router();
 
 userRouter.post("/",userRegistration)
+userRouter.post("/login",loginUser)
 
 
 export default userRouter;
